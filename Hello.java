135370f48f1ae146306a18e4e149f69a3dbd9b07
46353658 <student46353658@gmail.com>
2018-08-10 09:50:16 +0200

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello<T> {
+    T t;
+    public Hello(T t) { this.t = t; }
+    public String toString() { return t.toString(); }
+    public static void main(String[] args) {
+        System.out.print(new Hello<String>("hi "));
+        System.out.print(new Hello("there"));
+    }
+}
